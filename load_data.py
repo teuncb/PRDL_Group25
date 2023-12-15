7344b4c747f4097dc1257381822494d93d1d53d4
teuncb <50371179+teuncb@users.noreply.github.com>
2023-12-15 16:47:37 +0100

    minmax scaling with outlier business

--- load_data.py
+++ load_data.py
@@ -8,10 +8,29 @@ def get_dataset_name(file_name_with_dir):
 
 import os
 print(os.getcwd())
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
 
-filename_path = "MEG_data/Final Project data/Intra/train/rest_105923_1.h5"
+filename_path = "MEG_data/Final Project data/Intra/train/task_motor_105923_5.h5"
+# OUTLIER ALERT: in "MEG_data/Final Project data/Intra/train/task_motor_105923_5.h5" konden we niet de MinMaxScaler()
+# gebruiken, omdat dan meteen één outlier alles verder dichtbij 1 liet zitten
+
 with h5py.File(filename_path,'r') as f:
     dataset_name=get_dataset_name(filename_path)
     matrix=f.get(dataset_name)[()]
     print(type(matrix))
     print(matrix.shape)
+
+    print(matrix[:20])
+
+    mmscaler = MinMaxScaler()
+    mmscaler.fit(matrix[:300,:1000])
+    larger = mmscaler.transform(matrix[:300,:1000])
+    print(larger)
+
+    plt.imshow(larger)
+    plt.colorbar()
+    plt.show()
