a1f4605f636064f3a887dc3e642a433391fae902
teuncb <50371179+teuncb@users.noreply.github.com>
2023-12-15 15:55:07 +0100

    Code from assignment

---
+++ load_data.py
@@ -0,0 +1,13 @@
+import h5py
+def get_dataset_name(file_name_with_dir):
+    filename_without_dir = file_name_with_dir.split('/')[-1]
+    temp = filename_without_dir.split('')[:-1]
+    dataset_name="_".join(temp)
+    return dataset_name
+
+filename_path = "Intra/train/rest_105923_1.h5"
+with h5py.File(filename_path,'r') as f:
+    dataset_name=get_dataset_name(filename_path)
+    matrix=f.get(dataset_name)[()]
+    print(type(matrix))
+    print(matrix.shape)
