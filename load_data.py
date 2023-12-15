6a62fa01adefa46b72a2c9e5fa009e757a0c32e4
teuncb <50371179+teuncb@users.noreply.github.com>
2023-12-15 16:10:19 +0100

    Working code from assignment

--- load_data.py
+++ load_data.py
@@ -1,11 +1,15 @@
 import h5py
+
 def get_dataset_name(file_name_with_dir):
     filename_without_dir = file_name_with_dir.split('/')[-1]
-    temp = filename_without_dir.split('')[:-1]
+    temp = filename_without_dir.split('_')[:-1]
     dataset_name="_".join(temp)
     return dataset_name
 
-filename_path = "Intra/train/rest_105923_1.h5"
+import os
+print(os.getcwd())
+
+filename_path = "MEG_data/Final Project data/Intra/train/rest_105923_1.h5"
 with h5py.File(filename_path,'r') as f:
     dataset_name=get_dataset_name(filename_path)
     matrix=f.get(dataset_name)[()]
