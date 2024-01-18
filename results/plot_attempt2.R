7fc9e122149d14e69547d7f7cd58435178f6103b
teuncb <50371179+teuncb@users.noreply.github.com>
2024-01-18 17:31:42 +0100

    plot for new training loop

--- results/plot_attempt2.R
+++ results/plot_attempt2.R
@@ -3,7 +3,7 @@ setwd("!GitHub/PRDL_Group25/results")
 library(ggplot2)
 
 # Your data
-dat <- read.csv("val_set_results.csv", sep=";")
+dat <- read.csv("val_set_results_updated.csv", sep=";")
 
 dat_cross = dat[dat$data == "cross",]
 dat_intra = dat[dat$data == "intra",]
@@ -12,17 +12,16 @@ dat_intra = dat[dat$data == "intra",]
 # Plotting
 cbbPalette <- c("#000000", "#E69F00", "#56B4E9")
 
-ggplot(dat, aes(x = learning_rate, y = acc, color = factor(lstm_units), 
-                      shape = factor(timeframe), linetype = factor(timeframe))) +
+ggplot(dat, aes(x = learning_rate, y = acc, color = factor(timeframe), 
+                      shape = factor(lstm_units), linetype = factor(lstm_units))) +
   geom_point() +
   geom_line(aes(group = interaction(timeframe, lstm_units)), size=1) +
-  #scale_x_discrete(trans = log2_trans())
-  scale_x_discrete(labels=c('1e-4', '1e-3', '1e-2'), limits=rev) +
+  # scale_x_discrete(labels=c('1e-4', '1e-3', '1e-2'), limits=rev) +
   labs(x = "Learning Rate",
        y = "Accuracy",
-       color = "LSTM units",
-       linetype = "Timeframe",
-       shape = "Timeframe") +
+       color = "Timeframe",
+       linetype = "LSTM Units",
+       shape = "LSTM Units") +
   scale_colour_manual(values=cbbPalette) +
   theme_minimal() + 
   facet_grid(.~data)
